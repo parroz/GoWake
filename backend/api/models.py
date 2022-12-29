3e17f2e963285d88ec6ce5e5cf9056b57a6400e9
MMonteiro01 <mmonteiro@protonmail.com>
2022-12-29 20:25:26 +0000

    first commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,84 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    createAt = models.DateTimeField(auto_now=True)
+    updateAt = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class Competition(Base):
+    code = models.CharField(unique=True, max_length=15)
+    discipline = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    organizing_country = models.CharField(max_length=5)
+    tournament_type = models.CharField(max_length=10)
+    venue = models.CharField(max_length=100)
+    site_code = models.CharField(max_length=100)
+    age_groups = models.CharField(max_length=20)
+    beginning_date = models.DateTimeField(auto_now=False)
+    end_date = models.DateTimeField(auto_now=False)
+
+    class Meta:
+        verbose_name = 'Competition'
+        verbose_name_plural = 'Competitions'
+
+    def __str__(self):
+        return self.code
+
+
+class Event(Base):
+    id_competition = models.ForeignKey(Competition, related_name='events', on_delete=models.CASCADE)
+    rounds = models.IntegerField()
+    event_class = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+
+class Official(Base):
+    id_competition = models.ForeignKey(Competition, related_name='officials', on_delete=models.CASCADE)
+    iwwf_id = models.CharField(max_length=10)
+    position = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=10)
+    region = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Official'
+        verbose_name_plural = 'Officials'
+
+
+class Athlete(Base):
+    id_competition = models.ForeignKey(Competition, related_name='athletes', on_delete=models.CASCADE)
+    fed_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=10)
+    gender = models.CharField(max_length=1)
+    year_of_birth = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Athlete'
+        verbose_name_plural = 'Athletes'
+
+
+class AthleteEvent(Base):
+    id_athlete = models.ForeignKey(Athlete, related_name='athlete_events', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    division = models.CharField(max_length=20, blank=True)
+    entry_type = models.CharField(max_length=10)
+    participation = models.BooleanField(default=True)
+    real_category = models.CharField(max_length=5)
+    category_in_competition = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = 'AthleteEvent'
+        verbose_name_plural = 'AthleteEvents'
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,118 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import Competition, Official, Athlete, Event, AthleteEvent
+from rest_framework.serializers import Serializer, FileField
 
 User = get_user_model()
 
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = (
+            'id',
+            'id_competition',
+            'rounds',
+            'event_class',
+            'name',
+            'createAt',
+            'updateAt',
+            'username'
+        )
+
+
+class OfficialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Official
+        fields = (
+            'id',
+            'id_competition',
+            'iwwf_id',
+            'position',
+            'first_name',
+            'last_name',
+            'qualification',
+            'country',
+            'region',
+            'createAt',
+            'updateAt',
+            'username'
+        )
+
+
+class AthleteEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AthleteEvent
+        fields = (
+            'id',
+            'id_athlete',
+            'name',
+            'division',
+            'entry_type',
+            'participation',
+            'real_category',
+            'category_in_competition',
+            'createAt',
+            'updateAt',
+            'username'
+        )
+
+
+class AthleteSerializer(serializers.ModelSerializer):
+    athlete_events = AthleteEventSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Athlete
+        fields = (
+            'id',
+            'id_competition',
+            'fed_id',
+            'first_name',
+            'last_name',
+            'country',
+            'gender',
+            'year_of_birth',
+            'athlete_events',
+            'createAt',
+            'updateAt',
+            'username'
+        )
+
+
+class CompetitionSerializer(serializers.ModelSerializer):
+    events = EventSerializer(many=True, read_only=True)
+    officials = OfficialSerializer(many=True, read_only=True)
+    athletes = AthleteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Competition
+        fields = (
+            'id',
+            'code',
+            'discipline',
+            'name',
+            'age_groups',
+            'organizing_country',
+            'tournament_type',
+            'venue',
+            'site_code',
+            'beginning_date',
+            'events',
+            'officials',
+            'athletes',
+            'end_date',
+            'createAt',
+            'updateAt',
+            'username'
+        )
+
+
+"""
 class UserProductInlineSerializer(serializers.Serializer):
     url = serializers.HyperlinkedIdentityField(
-            view_name='product-detail',
-            lookup_field='pk',
-            read_only=True
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only=True
     )
     title = serializers.CharField(read_only=True)
 
@@ -17,16 +122,16 @@ class UserPublicSerializer(serializers.Serializer):
     this_is_not_real = serializers.CharField(read_only=True)
     id = serializers.IntegerField(read_only=True)
     # other_products = serializers.SerializerMethodField(read_only=True)
+"""
+# class Meta:
+#     model = User
+#     fields = [
+#         'username',
+#         'this_is_not_real',
+#         'id'
+#     ]
 
-    # class Meta:
-    #     model = User
-    #     fields = [
-    #         'username',
-    #         'this_is_not_real',
-    #         'id'
-    #     ]
-
-    # def get_other_products(self, obj):
-    #     user = obj
-    #     my_products_qs = user.product_set.all()[:5]
-    #     return UserProductInlineSerializer(my_products_qs, many=True, context=self.context).data
+# def get_other_products(self, obj):
+#     user = obj
+#     my_products_qs = user.product_set.all()[:5]
+#     return UserProductInlineSerializer(my_products_qs, many=True, context=self.context).data
