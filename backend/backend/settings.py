ee984483b8265bf3cc015f9930cd4537b0a0d152
MMonteiro01 <mmonteiro@protonmail.com>
2023-04-04 19:43:06 +0100

    cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ORIGIN_WHITELIST = [
     'https://gowake.daletech.pt',
+    'http://localhost:3000'
 ]
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
